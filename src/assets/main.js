895ed968f8a8e181f10e2d2917489430632475aa
simon.j.darlington <simon.j.darlington@MW7YZ6EJXMXN6Q.dir.svc.accenture.com>
2016-12-12 15:53:25 +0000

    finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
 
-  // your code will go here
+ $.ajax({
+    url: 'https://www.codeschool.com/users/1877865.json',
+    dataType: 'jsonp',
+    success: function(response) {
+        addCourses(response.courses.completed);
+    }
+  });
+
+    function addCourses(courses){
+
+        var badges = $("#badges");
+
+        courses.forEach(function(course) {
+
+            var courseholder = $("<div />", {
+              "class" : "course"
+            }).appendTo(badges);
+
+            $("<h3 />", {
+                text: course.title
+            }).appendTo(courseholder);
+
+            $("<img />", {
+                src: course.badge
+            }).appendTo(courseholder);
+
+            $("<a />", {
+                "class" : "btn btn-primary",
+                href: course.url,
+                target: "_blank",
+                text: "See Course"
+            }).appendTo(courseholder);
+
+        })
+    }
 
 });
